9302bf974a89dbbfa0faff870419f5c92aa4d01d
guptadkumar3672 <gupta.dkumar3672@gmail.com>
2025-06-24 14:57:50 +0530

    change color

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -63,6 +63,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#5F12AA',
     marginBottom: 15,
+     shadowColor: '#000', 
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
   },
   taskInfo: {
     flexDirection: 'row',
